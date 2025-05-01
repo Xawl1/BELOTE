f6ed2593eefc56bf1906efb8ebaa3265526b07a8
AntonioK <132918567+Xawl@users.noreply.github.com>
2025-05-01 15:42:02 +0300

    etio go
    e

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+099f06b7a39b9570924646470b60754f09ce9619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a61142ebb13f6dd98d36f34ff2033feb5d4481f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
